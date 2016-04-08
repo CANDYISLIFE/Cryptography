639e7a6abf0b2de6b2cf4abb289f936c32a51576
CANDYISLIFE <billybender@hanovernorwichschools.org>
2016-04-08 14:41:42 -0400

    Updated from Brython Server: 4/8/2016 2:41:42 PM

--- cryptography.py
+++ cryptography.py
@@ -22,51 +22,49 @@ cypherkeyNums = []
 decryptedNums = []
 decryptedLetters = ""
 end="false"
-
-if userInput != "e" and userInput != "d" and userInput != "q":
-    print("Did not understand command, try again. ")
-elif userInput == "q":
-    print("Goodbye!")
-    end = "true"
-elif userInput == "e":
-    userString = input("Message: ")
-    userKey = input("Key: ")
-    for i in userString:
-        stringInt.append(associations.find(i))
-    for e in userKey:
-        keyInt.append(associations.find(e))
-    while len(keyInt) <= len(stringInt):
-        keyInt = keyInt + keyInt
-    zippedNumbers = zip(stringInt, keyInt)
-    for p in zippedNumbers:
-        encryptedNumbers.append((p[0] + p[1]))
-    for t in encryptedNumbers:
-        if t > 85:
-            t = t - 85
-        encryptedLetters = encryptedLetters + associations[t]
-    print(encryptedLetters) 
-elif userInput == "d":
-    userCyphertext = input("Message: ")
-    cyphertextKey = input("Key: ")
-    for q in userCyphertext:
-        cyphertextNums.append(associations.find(q))
-    for y in cyphertextKey:
-        cypherkeyNums.append(associations.find(y))
-    while len(cyphertextNums) >= len(cypherkeyNums):
-        cypherkeyNums = cypherkeyNums + cypherkeyNums
-    zippedCypher = zip(cyphertextNums, cypherkeyNums)
-    for r in zippedCypher:
-        decryptedNums.append((r[0] - r[1]))
-    for h in decryptedNums:
-        if h < 0:
-            h = h + 85
-        decryptedLetters = decryptedLetters + associations[h]
-print(decryptedLetters)
-
-if userInput == "q":
-    end = "true"
 while end == "false":
     input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if userInput != "e" and userInput != "d" and userInput != "q":
+        print("Did not understand command, try again. ")
+    elif userInput == "q":
+        print("Goodbye!")
+        end = "true"
+    elif userInput == "e":
+        userString = input("Message: ")
+        userKey = input("Key: ")
+        for i in userString:
+            stringInt.append(associations.find(i))
+        for e in userKey:
+            keyInt.append(associations.find(e))
+        while len(keyInt) <= len(stringInt):
+            keyInt = keyInt + keyInt
+        zippedNumbers = zip(stringInt, keyInt)
+        for p in zippedNumbers:
+            encryptedNumbers.append((p[0] + p[1]))
+        for t in encryptedNumbers:
+            if t > 85:
+                t = t - 85
+            encryptedLetters = encryptedLetters + associations[t]
+        print(encryptedLetters) 
+    elif userInput == "d":
+        userCyphertext = input("Message: ")
+        cyphertextKey = input("Key: ")
+        for q in userCyphertext:
+            cyphertextNums.append(associations.find(q))
+        for y in cyphertextKey:
+            cypherkeyNums.append(associations.find(y))
+        while len(cyphertextNums) >= len(cypherkeyNums):
+            cypherkeyNums = cypherkeyNums + cypherkeyNums
+        zippedCypher = zip(cyphertextNums, cypherkeyNums)
+        for r in zippedCypher:
+            decryptedNums.append((r[0] - r[1]))
+        for h in decryptedNums:
+            if h < 0:
+                h = h + 85
+            decryptedLetters = decryptedLetters + associations[h]
+    print(decryptedLetters)
+
+
     
 
 """    
